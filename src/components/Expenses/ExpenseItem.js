99c93b9ada29e52a066f89a5e33260eecc9a189f
Archi <senarchi4@gmail.com>
2023-07-22 21:54:46 +0530

    List added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,10 +6,6 @@ import Card from '../UI/Card';
 
 const ExpenseItem=(props)=>{
     const [title,setTitle]=useState(props.title)
-
-    const clickHandler=()=>{
-        setTitle('Updated');
-    }
  
    
     return(
@@ -19,7 +15,6 @@ const ExpenseItem=(props)=>{
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Click Me</button>
         </Card>
     )
 }
